54711379e04a3542b97006068a560e2f729ef3c4
Minh Trung Pham <mpham@mines.edu>
2012-12-05 22:42:21 -0700

    Cleaned up character select screen, added next GameActivity, even though its unimplemented Added new asset for Megaman

--- android/src/android/Game/GameActivity.java
+++ android/src/android/Game/GameActivity.java
@@ -1,5 +1,6 @@
 package android.Game;
 
+import android.widget.HorizontalScrollView;
 import processing.core.*;
 
 public class GameActivity extends PApplet{
@@ -7,13 +8,21 @@ public class GameActivity extends PApplet{
 	//these will probably get merged to a Map class when holes etc get implemented
 	PImage background;
 	PImage foreground;
+	private static String bgImg = "a";
+	private static String fgImg = "asdas";
+	
 	
 	public void setup(){
-		
+		orientation(LANDSCAPE);
+		//background = loadImage(bgImg);
+		//foreground = loadImage(fgImg);
 	}
 	
 	public void draw(){
-		
+		background(0,0,0);
+		fill(255,255,255);
+		textSize(72);
+		text("STARTED",50,50);
 	}
 
 }
--- android/src/android/Game/sketch_121202c.java
+++ android/src/android/Game/sketch_121202c.java
@@ -6,84 +6,129 @@ import processing.opengl.*;
 
 import android.view.MotionEvent; 
 import android.view.KeyEvent; 
+import android.content.Intent;
 import android.graphics.Bitmap; 
 import java.io.*; 
 import java.util.*; 
+import sprites.*;
 
 public class sketch_121202c extends PApplet {
 
-ArrayList<PImage> imgsM = new ArrayList<PImage>();
-ArrayList<PImage> imgsQ = new ArrayList<PImage>();
-Playable player;
-Player player1;
-int counter = 0;
-
-public void setup() {
+	private static int characters = 2;
+	ArrayList<PImage> imgsM = new ArrayList<PImage>();
+	ArrayList<PImage> imgsQ = new ArrayList<PImage>();
 	
-	String s = "";
-	for(int i = 1; i < 6; i++) {
-		s = "p"+i+".png";
-		PImage img = loadImage(s);
-		imgsM.add(img);
-	}
-	for(int i = 1; i < 4; i++) {
-		s = "q"+i+".png";
-		PImage img = loadImage(s);
-		imgsQ.add(img);
-	}
-	player1 = new Player();
-	frameRate(10);
-}
+	PImage megamanImg, maskImg;
+	PGraphics pg;
+	PImage toDraw;
+	Playable player;
+	Player player1;
+	int counter = 0;
 
-public void draw() {
-	background(255,255,255);
-	//line(0,4*counter%900, 480, (4*counter - 150)%600);
+	public void setup() {
+		megamanImg = loadImage("megaman.png");
+		megamanImg.loadPixels();
+		orientation(LANDSCAPE);
+		String s = "";
+		for(int i = 1; i < 6; i++) {
+			s = "p"+i+".png";
+			PImage img = loadImage(s);
+			imgsM.add(img);
+		}
+		for(int i = 1; i < 4; i++) {
+			s = "q"+i+".png";
+			PImage img = loadImage(s);
+			imgsQ.add(img);
+		}
+		player1 = new Player();
+		frameRate(15);
+	}
+
+	void drawTitle() {
+		textSize(48);
+		fill(0,0,0);
+		text("Select Your Hero!!!", displayWidth/4 , displayHeight/12);
+	}
+
+	void drawCharacters() {
+		PImage temp = createImage(34, 44, ARGB);
+		temp = megamanImg.get(34*(counter%10), 0, 34, 44);
+		/*pg = createGraphics(displayWidth, displayHeight, P2D);
+		pg.beginDraw();
+		
+		pg.image(megamanImg, 34*(counter%10), 0, 34, 44);
+		//noFill();
+		//copy(megamanImg, 34*(counter%10), 0, 34, 44, displayWidth/16, displayHeight/12, displayHeight/4, displayHeight/4);
+		pg.endDraw();
+		image(pg,  displayWidth/16, displayHeight/12, displayHeight, displayHeight); 
+		*/
+		image(temp,  displayWidth/16, displayHeight/12, displayHeight/4, displayHeight/4); 
+	}
 	
-	if(player1 != null ) {
+	void drawStats() {
 		textSize(24);
 		fill(145, 255, 185);
-		rect(2, 550, 480, 355, 10);
+		rect(displayWidth/2+4, displayHeight/12+5, displayWidth/2-8, displayHeight/2, 10);
 		
 		fill(0, 0, 60);
-		text("Hero:", 5, 575);
-		text(player1.heroName, 145, 575);
+		text("Hero:", 3*displayWidth/6 + 8, 3*displayHeight/16);
+		text(player1.heroName, 4*displayWidth/6, 3*displayHeight/16);
 		
 		fill(230, 40, 10);
-		text("Health:", 5, 610);
-		rect(145, 600, player1.healthPoints*10, 15, 10);
+		text("Health:", 3*displayWidth/6 + 8, 4*displayHeight/16);
+		rect(4*displayWidth/6, 4*displayHeight/16-12, player1.healthPoints*10, 15, 10);
 		
 		fill(20, 230, 115);
-		text("Atk Pwr:", 5, 645);
-		rect(145, 635, player1.attackPower*10, 15, 10);
+		text("Atk Pwr:", 3*displayWidth/6 + 8, 5*displayHeight/16);
+		rect(4*displayWidth/6, 5*displayHeight/16-12, player1.attackPower*10, 15, 10);
 		
 		fill(0, 0, 0);
-		text("Ability:", 5, 680);
-		text(player1.ability, 145, 680);
-		
-		text("STAART!!", 160, 750);
-		//text(mouseX, 100, 700);
-		//text(mouseY, 200, 700);
+		text("Ability:", 3*displayWidth/6 + 8, 6*displayHeight/16);
+		text(player1.ability, 4*displayWidth/6, 6*displayHeight/16);
 	}
 	
-	
-	counter++;
-	
-	image(imgsM.get(counter%5), 50, 50, 100, 100);
-	image(imgsQ.get(counter%3), 50, 160, 100, 100);
-	
-	if (mousePressed) {
-		if (mouseY < 150) {
-			player1.pickMegaman();
-			text("Megaman", 200, 300);
-		} else if ( mouseY < 300) {
-			player1.pickQuagsire();
-			text("Quagsire", 200, 300);
+	void drawStartButton() {
+		textSize(36);
+		fill(45, 255, 185);
+		rect(5*displayWidth/8, 6*displayHeight/8, displayWidth/4, displayHeight/12, 10);
+	}
+	public void mousePressed(){
+		if((mouseY > 4*displayHeight/6 && mouseX < displayHeight)  &&(mouseX > 4*displayWidth/8 && mouseX < 7*displayWidth/8))
+		{
+			text("HEYHEY!", 50, 50);
+			Intent i = new Intent(this, GameActivity.class);
+			startActivity(i);
 		}
 	}
-
 	
-}
-
-  public int sketchWidth() { return 480; }
-  public int sketchHeight() { return 920; }
+	public void draw() {
+		background(255,255,255);
+		drawTitle();
+	
+		if(player1 != null ) {
+			drawStats();
+			drawStartButton();
+			text("STAART!!", displayWidth/2-30, 5*displayHeight/6);
+			//text(mouseX, 100, 700);
+			//text(mouseY, 200, 700);
+		}
+		
+		
+		counter++;
+		drawCharacters();
+		//image(imgsM.get(counter%5), displayWidth/16, displayHeight/12, displayHeight/4, displayHeight/4);
+		image(imgsQ.get(counter%3), displayWidth/16, (displayHeight/4)+displayHeight/12, displayHeight/4, displayHeight/4);
+		
+		if (mousePressed) {
+			if (mouseY < 150) {
+				player1.pickMegaman();
+				text("Megaman", 200, 300);
+			} else if ( mouseY < 300) {
+				player1.pickQuagsire();
+				text("Quagsire", 200, 300);
+			}
+		}
+	
+		
+	}
 }
